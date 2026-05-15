d8e418996fd9a1a9e85eed7e8158ba23065eb31c
Animesh <animesh96sharma@gmail.com>
2026-05-15 21:39:31 +0200

    Added new videos Dataset

--- backend/app/subtask1_segmentation/run_pipeline.py
+++ backend/app/subtask1_segmentation/run_pipeline.py
@@ -7,9 +7,9 @@ def run_pipeline(video_path):
     base_name = os.path.splitext(os.path.basename(video_path))[0]
 
     # Define paths
-    transcript_path = f"data/processed/subtask1_segmentation/transcripts/{base_name}_transcripts.json"
-    captions_path = f"data/processed/subtask1_segmentation/captions/{base_name}_captions.json"
-    chapters_path = f"data/processed/subtask1_segmentation/chapters/{base_name}_chapters.json"
+    transcript_path = f"/home/umwise2526studentproj/Group3ProjectWork/data/processed/subtask1_segmentation/transcripts/{base_name}_transcripts.json"
+    captions_path = f"/home/umwise2526studentproj/Group3ProjectWork/data/processed/subtask1_segmentation/captions/{base_name}_captions.json"
+    chapters_path = f"/home/umwise2526studentproj/Group3ProjectWork/data/processed/subtask1_segmentation/chapters/{base_name}_chapters.json"
 
     # Step 1: ASR
     print("Running ASR...")
